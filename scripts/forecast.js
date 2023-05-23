27f3e7d8c1be3bcbd528e1dbfb3808f5a416bd96
CocerhanVladConstantin <cocerhanvladc@gmail.com>
2023-05-23 21:53:29 +0300

    6.Weather-App

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = 'vyDi07LCcLZJotvTU9E26Loy9wppw8mQ';
+
+// get weather information 
+
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+//get city information
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
